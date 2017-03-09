942ea7c3a27664c1749f163ac8ddf43ea30b2288
lindsaymkelly <lindzlady@gmail.com>
2017-03-09 16:25:12 -0500

    Test out all boolean operators

--- basics/relational.java
+++ basics/relational.java
@@ -1,5 +1,10 @@
 public class RelationalOperators {
   public static void main(String[] args) {
 
+    System.out.println(13 <= 7);
+    System.out.println(7 < 13);
+    System.out.println(5 > 3);
+    System.out.println(2 >= 17);
+
   }
 }
